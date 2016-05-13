5f61a2e9073fc7beedc501a39b820c5b0d757af4
Manuel <manutepowa@gmail.com>
2016-05-13 12:18:07 +0200

    Audio y Salto de línea terminado

--- background.js
+++ background.js
@@ -26,12 +26,16 @@ var audio = chrome.contextMenus.create({
 function fSaltoLinea(data) {
 	// chrome.extension.sendMessage({ greeting: "hello" }, function (response) {});
 	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function (response) {
-			console.log(response.farewell);
+		chrome.tabs.sendMessage(tabs[0].id, { greeting: "sl" }, function (response) {
+			// console.log(response.farewell);
 		});
 	});
 }
 
 function fAudio(data) {
-	console.log(data);
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, { greeting: "ad" }, function (response) {
+			// console.log(response.farewell);
+		});
+	});
 }
--- js/script_page.js
+++ js/script_page.js
@@ -1,12 +1,25 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	console.log(request.greeting);
 	var saltosLinea = "&nbsp;</ br>&nbsp;</ br>&nbsp;</ br>\n";
+	var audio = "[audio: (URL de AUDIO) |titles= (TITULO) ]";
 
-	$('textarea#content').text(function () {
-		var cursorPos = $(this).prop('selectionStart');
-		var v = $(this).val();
-		var textBefore = v.substring(0, cursorPos);
-		var textAfter = v.substring(cursorPos, v.length);
-		$(this).val(textBefore + saltosLinea + textAfter);
-	});
+	if (request.greeting == "sl") {
+		$('textarea').text(function () {
+			var cursorPos = $(this).prop('selectionStart');
+			var v = $(this).val();
+			var textBefore = v.substring(0, cursorPos);
+			var textAfter = v.substring(cursorPos, v.length);
+			$(this).val(textBefore + saltosLinea + textAfter);
+		});
+	}
+	else if (request.greeting == "ad"){
+		$('textarea').text(function () {
+			var cursorPos = $(this).prop('selectionStart');
+			var v = $(this).val();
+			var textBefore = v.substring(0, cursorPos);
+			var textAfter = v.substring(cursorPos, v.length);
+			$(this).val(textBefore + audio + textAfter);
+		});
+	}
+
 });
