59888b435774bcff42f5c47e8af2d4dc909266a2
Manuel <manutepowa@gmail.com>
2016-05-13 18:05:48 +0200

    Logo añadido

--- js/script_page.js
+++ js/script_page.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	console.log(request.greeting);
 	var saltosLinea = "&nbsp;</ br>&nbsp;</ br>&nbsp;</ br>\n";
-	var audio = "[audio: (URL de AUDIO) |titles= (TITULO) ]";
+	var audio = "[audio:(((URL de AUDIO)))|titles=(((TITULO)))]";
 
 	if (request.greeting == "sl") {
 		$('textarea').text(function () {
@@ -11,8 +11,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 			var textAfter = v.substring(cursorPos, v.length);
 			$(this).val(textBefore + saltosLinea + textAfter);
 		});
-	}
-	else if (request.greeting == "ad"){
+	} else if (request.greeting == "ad") {
 		$('textarea').text(function () {
 			var cursorPos = $(this).prop('selectionStart');
 			var v = $(this).val();
--- popup.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  // $('h2').html('esesees');
-  $('button').click(function(){
-    $(document).css('background','red');
-    console.log($(document));
-  });
-});
